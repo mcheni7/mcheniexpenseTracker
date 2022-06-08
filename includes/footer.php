82fe497e5aa869ab5c41e5f0b489e4c7360dd39e
hussein mcheni <51811607+mcheni7@users.noreply.github.com>
2022-06-08 15:22:55 -0700

    Add files via upload
    mcheni expanse

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<div class="col-sm-12">
+				<p class="back-link">Mcheni7 Daily Expense<a href="https://mcheni7.blogsport.com">mcheni7</a></p>
+			</div>
---
+++ user-profile.php
@@ -0,0 +1,164 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['detsuid']==0)) {
+  header('location:logout.php');
+  } else{
+    if(isset($_POST['submit']))
+  {
+    $userid=$_SESSION['detsuid'];
+    $fullname=$_POST['fullname'];
+  $mobno=$_POST['contactnumber'];
+
+     $query=mysqli_query($con, "update tbluser set FullName ='$fullname', MobileNumber='$mobno' where ID='$userid'");
+    if ($query) {
+    $msg="User profile has been updated.";
+  }
+  else
+    {
+      $msg="Something Went Wrong. Please try again.";
+    }
+  }
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Daily Expense Tracker || User Profile</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	<link rel="stylesheet" href="main.css">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<?php include_once('includes/header.php');?>
+	<?php include_once('includes/sidebar.php');?>
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Profile</li>
+			</ol>
+		</div><!--/.row-->
+		
+		
+				<div class="" style="width: 50%; float: left;">
+		
+		<div class="row">
+			<div class="col-lg-12">
+			
+				
+				
+				<div class="panel panel-default" style="">
+					<div class="panel-heading">Profile</div>
+					<div class="panel-body">
+						<p style="font-size:16px; color:red" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
+						<div class="col-md-12">
+							 <?php
+$userid=$_SESSION['detsuid'];
+$ret=mysqli_query($con,"select * from tbluser where ID='$userid'");
+$cnt=1;
+while ($row=mysqli_fetch_array($ret)) {
+
+?>
+							<form role="form" method="post" action="">
+								<div class="form-group">
+									<label>Full Name</label>
+									<input class="form-control" type="text" value="<?php  echo $row['FullName'];?>" name="fullname" required="true">
+								</div>
+								<div class="form-group">
+									<label>Email</label>
+<input type="email" class="form-control" name="email" value="<?php  echo $row['Email'];?>" required="true" readonly="true">
+								</div>
+								
+								<div class="form-group">
+									<label>Mobile Number</label>
+									<input class="form-control" type="text" value="<?php  echo $row['MobileNumber'];?>" required="true" name="contactnumber" maxlength="10">
+								</div>
+								<div class="form-group">
+									<label>Registration Date</label>
+									<input class="form-control" name="regdate" type="text" value="<?php  echo $row['RegDate'];?>" readonly="true">
+								</div>
+								
+								<div class="form-group has-success">
+									<button type="submit" class="btn btn-primary" name="submit">Update</button>
+								</div>
+								
+								
+								</div>
+								<?php } ?>
+							</form>
+						</div>
+						
+					</div></div></div></div>
+             <div class="" style="float:right; width: 250px; margin-right: 150px;">
+              
+              <div class="card" style="height: 380px;" >
+                  <!--upload image-->
+                  
+        <form action="user-profile.php" method="post" enctype="multipart/form-data">
+          <h2 class="text-center mb-3 mt-3">Update profile</h2>
+          <?php if (!empty($msg)): ?>
+            <div class="alert <?php echo $msg_class ?>" role="alert">
+              <?php echo $msg; ?>
+            </div>
+          <?php endif; ?>
+          <div class="form-group text-center" style="position: relative;" >
+            <span class="img-div">
+              <div class="text-center img-placeholder"  onClick="triggerClick()">
+                <h4>Update image</h4>
+              </div>
+              <img src="images/avatar.jpg" onClick="triggerClick()" id="profileDisplay">
+            </span>
+            <input type="file" name="profileImage" required="" onChange="displayImage(this)" id="profileImage" class="form-control" style="display: none; ">
+            <label>Profile Image</label>
+          </div>
+          <div class="form-group" style="padding-top: 25px;">
+
+            <button type="submit" name="save_profile" class="btn btn-primary btn-block">Save profile</button>
+          </div>
+        </form>
+      
+
+
+
+
+<script src="scripts.js"></script>
+
+                </div>
+              </div>
+
+				</div><!-- /.panel-->
+
+			</div><!-- /.col-->
+			<?php include_once('includes/footer.php');?>
+		</div><!-- /.row -->
+	</div><!--/.main-->
+	
+<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/custom.js"></script>
+	
+</body>
+</html>
+<?php }  ?>
